248f47bb7520143277823c7b7910745395d92265
vladklen <Vlad6097944@gmail.com>
2022-05-29 18:56:56 +0300

    Object-2 start

--- js/script.js
+++ js/script.js
@@ -8,37 +8,22 @@ burger.onclick = function () {
 	bodyLock.classList.toggle("lock");
 }
 
-const cardsItem = document.querySelectorAll('.default');
-for (let i = 0; i < cardsItem.length; i++) {
-	const cardItem = cardsItem[i];
-	cardItem.addEventListener('mousemove', startRotate);
-	cardItem.addEventListener('mouseout', stopRotate);
-}
+// const cardsItem = document.querySelectorAll('.card__item');
+// for (let i = 0; i < cardsItem.length; i++) {
+// 	const cardItem = cardsItem[i];
+// 	cardItem.addEventListener('mousemove', startRotate);
+// 	cardItem.addEventListener('mouseout', stopRotate);
+// }
 
-function startRotate(event) {
-	const card = this.querySelector('.usual');
-	const halfHeight = card.offsetHeight / 2;
-	const halfWidth = card.offsetWidth / 2;
-	card.style.transform = 'rotateX(' + -(event.offsetY - halfHeight) / 15 + 'deg) rotateY(' + (event.offsetX - halfWidth) / 15 + 'deg)';
-}
+// function startRotate(event) {
+// 	const card = this.querySelector('.card');
+// 	const halfHeight = card.offsetHeight / 2;
+// 	const halfWidth = card.offsetWidth / 2;
+// 	card.style.transform = 'rotateX(' + -(event.offsetY - halfHeight) / 15 + 'deg) rotateY(' + (event.offsetX - halfWidth) / 15 + 'deg)';
+// }
 
 
-
-const cardsHorizontal = document.querySelectorAll('.horizontal');
-for (let i = 0; i < cardsHorizontal.length; i++) {
-	const cardHorizontal = cardsHorizontal[i];
-	cardHorizontal.addEventListener('mousemove', RotateLong);
-	cardHorizontal.addEventListener('mouseout', stopRotate);
-}
-
-function RotateLong(event) {
-	const cardLong = this.querySelector('.long');
-	const halfHeight = card.offsetHeight / 2;
-	const halfWidth = card.offsetWidth / 2;
-	cardLong.style.transform = 'rotateX(' + -(event.offsetY - halfHeight) / 5 + 'deg) rotateY(' + (event.offsetX - halfWidth) / 5 + 'deg)';
-}
-
-function stopRotate(event) {
-	const card = this.querySelector('.card');
-	card.style.transform = 'rotate(0)';
-}
+// function stopRotate(event) {
+// 	const card = this.querySelector('.card');
+// 	card.style.transform = 'rotate(0)';
+// }
