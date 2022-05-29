9821a075305fc8ee88f7f71be7a19bd5d80b2b45
vladklen <Vlad6097944@gmail.com>
2022-05-29 14:15:19 +0300

    objects  js

--- js/script.js
+++ js/script.js
@@ -8,10 +8,37 @@ burger.onclick = function () {
 	bodyLock.classList.toggle("lock");
 }
 
-const card = document.querySelector('.objects__cards');
+const cardsItem = document.querySelectorAll('.default');
+for (let i = 0; i < cardsItem.length; i++) {
+	const cardItem = cardsItem[i];
+	cardItem.addEventListener('mousemove', startRotate);
+	cardItem.addEventListener('mouseout', stopRotate);
+}
 
-card.addEventListener('mousemove', rotate);
+function startRotate(event) {
+	const card = this.querySelector('.usual');
+	const halfHeight = card.offsetHeight / 2;
+	const halfWidth = card.offsetWidth / 2;
+	card.style.transform = 'rotateX(' + -(event.offsetY - halfHeight) / 15 + 'deg) rotateY(' + (event.offsetX - halfWidth) / 15 + 'deg)';
+}
 
-function rotate(event) {
-	console.log('Наведение мышкой');
+
+
+const cardsHorizontal = document.querySelectorAll('.horizontal');
+for (let i = 0; i < cardsHorizontal.length; i++) {
+	const cardHorizontal = cardsHorizontal[i];
+	cardHorizontal.addEventListener('mousemove', RotateLong);
+	cardHorizontal.addEventListener('mouseout', stopRotate);
+}
+
+function RotateLong(event) {
+	const cardLong = this.querySelector('.long');
+	const halfHeight = card.offsetHeight / 2;
+	const halfWidth = card.offsetWidth / 2;
+	cardLong.style.transform = 'rotateX(' + -(event.offsetY - halfHeight) / 5 + 'deg) rotateY(' + (event.offsetX - halfWidth) / 5 + 'deg)';
+}
+
+function stopRotate(event) {
+	const card = this.querySelector('.card');
+	card.style.transform = 'rotate(0)';
 }
