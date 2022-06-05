c1bf6c9f16299b2d240a8a1f896c4b4e3180b9a5
vladklen <Vlad6097944@gmail.com>
2022-06-05 15:33:39 +0300

    0506

--- js/script.js
+++ js/script.js
@@ -8,22 +8,41 @@ burger.onclick = function () {
 	bodyLock.classList.toggle("lock");
 }
 
-// const cardsItem = document.querySelectorAll('.card__item');
-// for (let i = 0; i < cardsItem.length; i++) {
-// 	const cardItem = cardsItem[i];
-// 	cardItem.addEventListener('mousemove', startRotate);
-// 	cardItem.addEventListener('mouseout', stopRotate);
-// }
+// Код для анимациии при прокрутке страницы
 
-// function startRotate(event) {
-// 	const card = this.querySelector('.card');
-// 	const halfHeight = card.offsetHeight / 2;
-// 	const halfWidth = card.offsetWidth / 2;
-// 	card.style.transform = 'rotateX(' + -(event.offsetY - halfHeight) / 15 + 'deg) rotateY(' + (event.offsetX - halfWidth) / 15 + 'deg)';
-// }
+const animItems = document.querySelectorAll('._anim-items');
 
+if (animItems.length > 0) {
+	window.addEventListener('scroll', animOnScroll);
+	function animOnScroll(params) {
+		for (let index = 0; index < animItems.length; index++) {
+			const animItem = animItems[index];
+			const animItemHeight = animItem.offsetHeight;
+			const animItemOffset = offset(animItem).top;
+			const animStart = 4;
 
-// function stopRotate(event) {
-// 	const card = this.querySelector('.card');
-// 	card.style.transform = 'rotate(0)';
-// }
+			let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+			if (animItemHeight > window.innerHeight) {
+				animItemPoint = window.innerHeight - window.innerHeight / animStart;
+			}
+
+			if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItem)) {
+				animItem.classList.add('_active');
+			} else {
+				if (!animItem.classList.contains('_anim-stop')) {
+					animItem.classList.remove('_active');
+				}
+			}
+		}
+	}
+	function offset(el) {
+		const rect = el.getBoundingClientRect(),
+			scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+			scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+		return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+	}
+	setTimeout(() => {
+		animOnScroll();
+	}, 300);
+}
