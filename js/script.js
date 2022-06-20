c94a968dce777506d92851591d65dff12aa8519f
vladklen <Vlad6097944@gmail.com>
2022-06-21 00:25:05 +0300

    slider

--- js/script.js
+++ js/script.js
@@ -70,29 +70,33 @@ jQuery(function ($) {
 			arrows: true,
 			speed: 300,
 			slidesToShow: 3,
-			slidesToScroll: 4,
+			slidesToScroll: 2,
+			autoplay: true,
 			responsive: [
 				{
 					breakpoint: 1024,
 					settings: {
-						slidesToShow: 3,
-						slidesToScroll: 3,
+						slidesToShow: 2,
+						slidesToScroll: 2,
 						infinite: true,
 						dots: true
 					}
 				},
 				{
-					breakpoint: 600,
+					breakpoint: 768,
 					settings: {
 						slidesToShow: 2,
-						slidesToScroll: 2
+						slidesToScroll: 2,
+						arrows: false,
 					}
 				},
 				{
-					breakpoint: 480,
+					breakpoint: 620,
 					settings: {
 						slidesToShow: 1,
-						slidesToScroll: 1
+						slidesToScroll: 1,
+						dots: false,
+						arrows: true,
 					}
 				}
 				// You can unslick at a given breakpoint now by adding:
