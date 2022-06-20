af64dba3819d7dcc52b6b4a67d5e5f27f323798f
vladklen <Vlad6097944@gmail.com>
2022-06-21 00:11:31 +0300

    2106

--- js/script.js
+++ js/script.js
@@ -55,3 +55,50 @@ if (animItems.length > 0) {
 		animOnScroll();
 	}, 300);
 }
+
+
+// JQuerry
+jQuery(function ($) {
+	if ($) {
+		console.log("jQuery start");
+	}
+
+	if ($(".main__slider").length > 0) {
+		$('.main__slider').slick({
+			dots: true,
+			infinite: true,
+			arrows: true,
+			speed: 300,
+			slidesToShow: 3,
+			slidesToScroll: 4,
+			responsive: [
+				{
+					breakpoint: 1024,
+					settings: {
+						slidesToShow: 3,
+						slidesToScroll: 3,
+						infinite: true,
+						dots: true
+					}
+				},
+				{
+					breakpoint: 600,
+					settings: {
+						slidesToShow: 2,
+						slidesToScroll: 2
+					}
+				},
+				{
+					breakpoint: 480,
+					settings: {
+						slidesToShow: 1,
+						slidesToScroll: 1
+					}
+				}
+				// You can unslick at a given breakpoint now by adding:
+				// settings: "unslick"
+				// instead of a settings object
+			]
+		});
+	}
+});
