00ba0f847fb20afcfdf37df3b4614538a3eebb0b
vladklen <Vlad6097944@gmail.com>
2022-06-23 20:29:49 +0300

    2306

--- js/script.js
+++ js/script.js
@@ -5,39 +5,6 @@ window.onload = function () {
 		document.body.classList.add('loaded');
 		document.body.classList.remove('loaded_hiding');
 	}, 500);
-	if (animItems.length > 0) {
-		window.addEventListener('scroll', animOnScroll);
-		function animOnScroll() {
-			for (let index = 0; index < animItems.length; index++) {
-				const animItem = animItems[index];
-				const animItemHeight = animItem.offsetHeight;
-				const animItemOffset = offset(animItem).top;
-				const animStart = 4;
-
-				let animItemPoint = window.innerHeight - animItemHeight / animStart;
-				if (animItemHeight > window.innerHeight) {
-					animItemPoint = window.innerHeight - window.innerHeight / animStart;
-				}
-
-				if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
-					animItem.classList.add('_animation');
-				} else {
-					if (!animItem.classList.contains('_anim-stop')) {
-						animItem.classList.remove('_animation')
-					}
-				}
-			}
-		}
-		function offset(el) {
-			const rect = el.getBoundingClientRect(),
-				scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-				scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-			return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
-		}
-		setTimeout(() => {
-			animOnScroll();
-		}, 800);
-	};
 }
 
 // Burger
@@ -54,7 +21,39 @@ burger.onclick = function () {
 // Код для анимациии при прокрутке страницы
 
 const animItems = document.querySelectorAll('._anim-items');
+if (animItems.length > 0) {
+	window.addEventListener('scroll', animOnScroll);
+	function animOnScroll() {
+		for (let index = 0; index < animItems.length; index++) {
+			const animItem = animItems[index];
+			const animItemHeight = animItem.offsetHeight;
+			const animItemOffset = offset(animItem).top;
+			const animStart = 4;
 
+			let animItemPoint = window.innerHeight - animItemHeight / animStart;
+			if (animItemHeight > window.innerHeight) {
+				animItemPoint = window.innerHeight - window.innerHeight / animStart;
+			}
+
+			if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+				animItem.classList.add('_animation');
+			} else {
+				if (!animItem.classList.contains('_anim-stop')) {
+					animItem.classList.remove('_animation')
+				}
+			}
+		}
+	}
+	function offset(el) {
+		const rect = el.getBoundingClientRect(),
+			scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+			scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+		return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+	}
+	setTimeout(() => {
+		animOnScroll();
+	}, 800);
+};
 
 
 
